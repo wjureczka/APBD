31b1410c8139dc033c2e5f1acc1328e37659f4de
Wojciech Jureczka <wjureczka@gmail.com>
2020-03-01 17:52:05 +0100

    Build: Get pjatk and find emails

--- APBD/Program.cs
+++ APBD/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace APBD
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var url = "https://www.pja.edu.pl/";
+
+            await Program.GetWebPage(url);
         }
+
+        public static async Task GetWebPage(String url)
+        {
+            using var httpClient = new HttpClient();
+
+            var pageSource = await httpClient.GetStringAsync(url);
+
+            var emailRegex = @"(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                             + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                             + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                             + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})";
+            
+            var mathResult = Regex.Matches(pageSource, emailRegex);
+
+            foreach (var email in mathResult)
+            {
+                Console.WriteLine(email);
+            }
+        } 
     }
 }
