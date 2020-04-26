5f5b35805e4983b92232c0a596eb9fe28cdb6d67
Wojciech Jureczka <wjureczka@gmail.com>
2020-04-26 22:01:51 +0200

    Done excercise 2

--- APBD/Controllers/EnrollmentsController.cs
+++ APBD/Controllers/EnrollmentsController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using APBD.DAL;
@@ -20,6 +21,64 @@ namespace APBD.Controllers
             _dbService = service;
         }
 
+        [HttpPost("promotions")]
+        public IActionResult PromoteStudent(PromoteStudentRequest request)
+        {
+            using (var connection =
+                new SqlConnection("Data Source=db-mssql;Initial Catalog=s17082;Integrated Security=True"))
+            using (var command = new SqlCommand())
+            {
+                command.Connection = connection;
+                command.Connection.Open();
+
+                command.Transaction = command.Connection.BeginTransaction();
+
+                try
+                {
+                    command.CommandText = "SELECT * FROM ENROLLMENT WHERE IdStudy = (SELECT IdStudy FROM STUDIES WHERE NAME = @StudyName) AND Semester = @Semester";
+                    command.Parameters.AddWithValue("StudyName", request.Studies);
+                    command.Parameters.AddWithValue("Semester", request.Semester);
+
+                    var reader = command.ExecuteReader();
+
+                    if (!reader.Read())
+                    {
+                        reader.Close();
+                        return NotFound();
+                    }
+
+                    reader.Close();
+                    command.Parameters.Clear();
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error);
+                    command.Transaction.Rollback();
+                    return BadRequest();
+                }
+
+                try
+                {
+                    command.CommandText = "PromoteStudents";
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@StudiesName", request.Studies);
+                    command.Parameters.AddWithValue("@OldSemester", request.Semester);
+                    
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error);
+                    command.Transaction.Rollback();
+                    return BadRequest();
+                }
+                
+                command.Transaction.Commit();
+
+                return Ok();
+            }
+        }
+
         [HttpPost]
         public IActionResult EnrollStudent(EnrollStudentRequest request)
         {
@@ -34,7 +93,7 @@ namespace APBD.Controllers
             EnrollStudentResponse response = new EnrollStudentResponse();
 
             Study study = this._dbService.GetStudy(request.Studies);
-            
+
             if (study == null)
             {
                 return BadRequest("Study unavailable");
@@ -42,32 +101,34 @@ namespace APBD.Controllers
 
             DateTime enrollmentStartDate = DateTime.Now;
 
-            using (var connection = new SqlConnection("Data Source=db-mssql;Initial Catalog=s17082;Integrated Security=True"))
+            using (var connection =
+                new SqlConnection("Data Source=db-mssql;Initial Catalog=s17082;Integrated Security=True"))
             using (var command = new SqlCommand())
             {
-                
                 command.Connection = connection;
                 command.Connection.Open();
                 var transaction = command.Connection.BeginTransaction();
                 command.Transaction = transaction;
 
-                
+
                 try
                 {
-                    command.CommandText = "SELECT IdEnrollment FROM ENROLLMENT WHERE IdStudy = @IdStudy AND Semester = 1";
+                    command.CommandText =
+                        "SELECT IdEnrollment FROM ENROLLMENT WHERE IdStudy = @IdStudy AND Semester = 1";
                     command.Parameters.AddWithValue("IdStudy", study.IdStudy);
 
                     var reader = command.ExecuteReader();
 
                     if (!reader.Read())
                     {
-                        command.CommandText = "INSERT INTO ENROLLMENT (Semester, IdStudy, StartDate) VALUES (1, @IdStudy, @StartDate)";
+                        command.CommandText =
+                            "INSERT INTO ENROLLMENT (Semester, IdStudy, StartDate) VALUES (1, @IdStudy, @StartDate)";
                         command.Parameters.AddWithValue("IdStudy", study.IdStudy);
                         command.Parameters.AddWithValue("StartDate", enrollmentStartDate);
 
                         command.ExecuteNonQuery();
                     }
-                    
+
                     reader.Close();
                 }
                 catch (Exception error)
@@ -90,7 +151,7 @@ namespace APBD.Controllers
                         transaction.Rollback();
                         return BadRequest("Student already exists");
                     }
-                    
+
                     reader.Close();
                 }
                 catch (Exception error)
@@ -102,7 +163,8 @@ namespace APBD.Controllers
 
                 try
                 {
-                    command.CommandText = "SELECT IdEnrollment FROM ENROLLMENT WHERE IdStudy = @IdStudy AND Semester = 1";
+                    command.CommandText =
+                        "SELECT IdEnrollment FROM ENROLLMENT WHERE IdStudy = @IdStudy AND Semester = 1";
 
                     var reader = command.ExecuteReader();
 
@@ -116,13 +178,14 @@ namespace APBD.Controllers
                     var enrollmentId = (int) reader["IdEnrollment"];
                     reader.Close();
 
-                    command.CommandText = "INSERT INTO STUDENT (IndexNumber, FirstName, LastName, BirthDate, IdEnrollment) VALUES (@IndexNumber, @FirstName, @LastName, @BirthDate, @IdEnrollment)";
+                    command.CommandText =
+                        "INSERT INTO STUDENT (IndexNumber, FirstName, LastName, BirthDate, IdEnrollment) VALUES (@IndexNumber, @FirstName, @LastName, @BirthDate, @IdEnrollment)";
                     command.Parameters.AddWithValue("IndexNumber", student.IndexNumber);
                     command.Parameters.AddWithValue("FirstName", student.FirstName);
                     command.Parameters.AddWithValue("LastName", student.LastName);
                     command.Parameters.AddWithValue("BirthDate", student.BirthDate);
                     command.Parameters.AddWithValue("IdEnrollment", enrollmentId);
-                    
+
                     command.ExecuteNonQuery();
                 }
                 catch (Exception error)
@@ -131,7 +194,7 @@ namespace APBD.Controllers
                     Console.WriteLine(error);
                     return BadRequest("Could not insert new student");
                 }
-                
+
                 transaction.Commit();
             }
 
